9d5e7615ee4b40bcd24ff490858dba6e2ce7a25b
Moritz Stephan <moritz.stephan@stanford.edu>
2024-06-02 22:36:02 -0700

    added eval script

--- cs336_alignment/eval.py
+++ cs336_alignment/eval.py
@@ -26,7 +26,7 @@ Your response must be socially responsible, and thus you can reject to answer so
 mmlu_prompt = """\
 # Instruction
 Below is a list of conversations between a human and an AI assistant (you).
-Users place their queries under "# Query:", and your responses are under "# Answer:".
+Users place their queries under "# Question:", and your responses are under "# Answer:".
 You are a helpful, respectful, and honest assistant.
 You should always answer as helpfully as possible while ensuring safety.
 Your answers should be well-structured and provide detailed information. They should also have an engaging tone.
@@ -34,12 +34,12 @@ Your responses must not contain any fake, harmful, unethical, racist, sexist, to
 Your response must be socially responsible, and thus you can reject to answer some controversial topics.
 Answer the following multiple choice question about {subject}. Respond with a single sentence of the form "The correct answer is _", filling the blank with the letter corresponding to the correct answer (i.e., A, B, C or D).
 
-Question: {question}
+# Question: {question}
 A. {options[0]}
 B. {options[1]}
 C. {options[2]}
 D. {options[3]}
-Answer: """
+# Answer:"""
 
 
 def load_mmlu_prompts(split: str = "test", path: str = "data/mmlu") -> list[tuple[str, str]]:
