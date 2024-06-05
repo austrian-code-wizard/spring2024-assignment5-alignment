edfbcc04954378439660d0ba81fc7ab4bd05b453
Moritz Stephan <moritz.stephan@stanford.edu>
2024-06-04 23:23:28 -0700

    added eval script

--- cs336_alignment/eval.py
+++ cs336_alignment/eval.py
@@ -196,7 +196,7 @@ def score_alpaca_response_batch(data: dict[str, str | int | None], model_name: s
     with open("results/alpaca_scores.json", "w+") as f:
         json.dump(scores, f, indent=2)
 
-    command = "conda activate cs336_alignment && alpaca_eval --model_outputs results/alpaca_scores.json --annotators_config 'scripts/alpaca_eval_vllm_llama3_70b_fn' --base-dir 'results'"
+    command = "conda init && conda activate cs336_alignment && alpaca_eval --model_outputs results/alpaca_scores.json --annotators_config 'scripts/alpaca_eval_vllm_llama3_70b_fn' --base-dir 'results'"
     subprocess.run(command, shell=True, check=True)
 
 
